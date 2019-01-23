fbe8d7ff14a72e6808d2147bf071ee8bbcc1c2f7
kingsdtoa <46977390+kingsdtoa@users.noreply.github.com>
2019-01-23 23:02:49 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("536844064990035968")
+setInterval(function() {
+channel.send(`zfgs25`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
